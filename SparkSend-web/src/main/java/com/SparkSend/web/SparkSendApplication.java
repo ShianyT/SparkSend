07436133c59342be38dd25f52001816b1b66ff8d
shiany <3297313741@qq.com>
2024-07-09 22:27:46 +0800

    创建springboot项目

---
+++ SparkSend-web/src/main/java/com/SparkSend/web/SparkSendApplication.java
@@ -0,0 +1,11 @@
+package com.SparkSend.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkSendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SparkSendApplication.class,args);
+    }
+}
