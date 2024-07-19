af89d9372292d2f22f729e04fdb6c0a480657481
shiany <3297313741@qq.com>
2024-07-19 11:33:35 +0800

    接入腾讯sdk，实现下发短信

--- SparkSend-web/src/main/java/com/SparkSend/web/TestController.java
+++ SparkSend-web/src/main/java/com/SparkSend/web/TestController.java
@@ -1,6 +1,5 @@
 package com.SparkSend.web;
 
-import lombok.extern.java.Log;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
