4c570316e88b73a803f5e2d2c91618e94218e050
shiany <3297313741@qq.com>
2024-07-19 09:58:16 +0800

    引入一些工具依赖包，并引入logback日志框架

--- SparkSend-web/src/main/java/com/SparkSend/SparkSendApplication.java
+++ SparkSend-web/src/main/java/com/SparkSend/SparkSendApplication.java
@@ -1,4 +1,4 @@
-package com.SparkSend.web;
+package com.SparkSend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ SparkSend-web/src/main/java/com/SparkSend/web/TestController.java
@@ -0,0 +1,17 @@
+package com.SparkSend.web;
+
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class TestController {
+
+    @RequestMapping("/test")
+    public String test(){
+        log.info("log：日志测试输出");
+        return "测试";
+    }
+}
